2ae4a20b3c026497ef0a08130af2a0d23eace561
dongbaram <dongbaram@naver.com>
2019-05-14 14:42:42 +0900

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,12 @@
 import os, json
 from flask import Flask, request, redirect, url_for
 from werkzeug import secure_filename
+
+UPLOAD_FOLDER = './uploads'
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+
 application = Flask(__name__)
+application.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @application.route("/")
 def hello():
