51e1c7a8259ffa5feb79f053daa03806bdb8a6d5
dongbaram <dongbaram@naver.com>
2019-05-14 15:04:39 +0900

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -16,18 +16,25 @@ def allowed_file(filename):
 def hello():
     return "OpenShift Hello World!"
 
+
 @application.route('/fileUpload', methods=['POST'])
 def upload_file(): 
+    #업로드 경로가 존재하는지 확인 - 없으면 생성함
+    if not os.path.isdir(application.config['UPLOAD_FOLDER']):
+        os.mkdir(application.config['UPLOAD_FOLDER'])
+
     file = request.files['ocrfile']
+
+    #jsontmp = request.get_json(silent=True, cache=False, force=True)
+    #print(request)
 
     if file and allowed_file(file.filename):
         filename = secure_filename(file.filename)
         file.save(os.path.join(application.config['UPLOAD_FOLDER'], filename))
-
+        #return redirect(url_for('uploaded_file', filename=filename))
         print('debug1')
         return 'uploaded'
     return 'error'
-
 
 if __name__ == "__main__":
     application.run()
