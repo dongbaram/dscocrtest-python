57cac40ec9d1107fbeeebb54a7435e3a28d428b9
dongbaram <dongbaram@naver.com>
2019-05-14 11:32:37 +0900

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -2,9 +2,35 @@ import os, json
 from flask import Flask, request, redirect, url_for
 from werkzeug import secure_filename
 
+UPLOAD_FOLDER = './uploads'
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
 @app.route("/")
 def hello():
     return "Hello World!"
+
+@app.route('/fileUpload', methods=['POST'])
+def upload_file(): 
+    file = request.files['ocrfile']
+
+    #jsontmp = request.get_json(silent=True, cache=False, force=True)
+    #print(request)
+
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        #return redirect(url_for('uploaded_file', filename=filename))
+        print('test debug')
+        return 'uploaded'
+    return 'error'
+
 
 if __name__ == "__main__":
     application.run()
