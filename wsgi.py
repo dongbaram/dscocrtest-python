a814e7bf88a03cc38f6d77bfddc320c140601cac
dongbaram <dongbaram@naver.com>
2019-05-14 14:26:30 +0900

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -6,8 +6,6 @@ UPLOAD_FOLDER = './uploads'
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-
 
 if __name__ == "__main__":
     application.run()
