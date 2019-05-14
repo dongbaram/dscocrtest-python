0130a75834a530b927ea2cb6ac1b6ec67ffbb21b
dongbaram <dongbaram@naver.com>
2019-05-14 14:28:25 +0900

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -2,9 +2,6 @@ import os, json
 from flask import Flask, request, redirect, url_for
 from werkzeug import secure_filename
 
-UPLOAD_FOLDER = './uploads'
-ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
-
 app = Flask(__name__)
 
 if __name__ == "__main__":
