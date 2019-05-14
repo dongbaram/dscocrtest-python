691ff190ebfe531fa380fab36345f2feb0bdd0e6
dongbaram <dongbaram@naver.com>
2019-05-14 17:11:39 +0900

    Update runocr_tmp2.py

--- runocr_tmp2.py
+++ runocr_tmp2.py
@@ -1,8 +1,10 @@
 
-
+#python 간 파라미터 테스트
 def main(data):
 
-    print(data["filename"])
+    print("main:"+data["filename"])  # 받은 파라미터
+    return_data = {"key1":"ok","key2":"main:"+data["filename"]} #return 도 dict type으로 전송
+    return return_data
 
 if __name__ == "__main__":
     data = {"filename":"aaaaa","key2":"v2"}
